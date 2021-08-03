5df90f25a97f6c761d9eef36096512c10dcdb979
AndreR2802 <55300275+AndreCin2802@users.noreply.github.com>
2021-08-02 22:48:01 -0300

    Remove Lombook

--- src/main/java/com/example/restsonda/Model/Empresa.java
+++ src/main/java/com/example/restsonda/Model/Empresa.java
@@ -1,17 +1,9 @@
 package com.example.restsonda.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,9 +20,66 @@ public class Empresa {
 
     @Column
     private String cnpj;
+    
+    
+
+	public Empresa() {
+		
+	}
 
 
-    @Override
+	public Empresa(Long id, String nome, String email, String empresa, String cnpj) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.empresa = empresa;
+		this.cnpj = cnpj;
+	}
+
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+
+	@Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
